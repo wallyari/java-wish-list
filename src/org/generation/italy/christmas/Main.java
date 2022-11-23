f26937e31d5233c87f991cd26a763a585c77016b
ariannadepasquale <depasqualearianna@gmail.com>
2022-11-23 15:53:43 +0100

    Comparator Test

--- src/org/generation/italy/christmas/Main.java
+++ src/org/generation/italy/christmas/Main.java
@@ -2,10 +2,43 @@ package org.generation.italy.christmas;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+	
+	
+	public static class VowelsComparator implements Comparator<String>{
+
+ 		@Override
+ 		public int compare(String o1, String o2) {
+ 
+ 			int v1 = 0 ;
+ 			int v2 = 0 ;
+ 			
+ 			for (int i = 0; i < o1.length(); i++) {
+ 
+ 				if(o1.charAt(i) == 'a' || o1.charAt(i) == 'e' || o1.charAt(i) == 'i' || o1.charAt(i) == 'o' 
+ 						|| o1.charAt(i) == 'u' ) {
+ 		
+ 					v1++;
+ 				}
+ 			}
+ 
+ 			for (int i = 0; i < o2.length(); i++) {
+
+ 				if(o2.charAt(i) == 'a' || o2.charAt(i) == 'e' || o2.charAt(i) == 'i' || o2.charAt(i) == 'o' 
+ 						|| o2.charAt(i) == 'u' ) {
+ 					v2++;
+ 				}
+ 			}
+ 			// TODO Auto-generated method stub
+ 			return v2- v1;
+ 		}
+ 	}
+	
+	
 
 	public static void main(String[] args) {
 		
@@ -32,5 +65,16 @@ public class Main {
 		Collections.sort(wishList);
 		System.out.println("La lista dei tuoi desideri: " + wishList);
 		
+ 		
+		System.out.println("- - Comparator - -");
+ 		VowelsComparator vowelsComparator = new VowelsComparator();
+ 		wishList.sort(vowelsComparator);
+ 		System.out.println(wishList);
+ 		
+ 		
+ 		sc.close();
+		
 	}
+	
+	
 }
